6e3eb53dda6eae7f10a03ac1194dd6c9cfaa49ce
Mike Farah <mike.farah@orchestrated.io>
2016-10-03 13:33:24 +1100

    Extracted out failure styles to less

--- common/components/Failure.js
+++ common/components/Failure.js
@@ -1,24 +1,12 @@
 import React, { PropTypes } from 'react';
-import reactCSS from 'reactcss';
 
-const Failure = ({ name, url }) => {
-  const styles = reactCSS({
-    default: {
-      link: {
-        textDecoration: 'none',
-        color: 'white',
-        fontSize: '24px',
-        fontFamily: 'Arial, Helvetica, sans-serif',
-      },
-    },
-  });
-
-  return <li>
-           <a href={ url } style={ styles.link }>
-             { name }
-           </a>
-         </li>;
-};
+const Failure = ({ name, url }) => (
+  <li className='failure'>
+    <a href={ url }>
+      { name }
+    </a>
+  </li>
+);
 
 Failure.propTypes = {
   name: PropTypes.string.isRequired,
