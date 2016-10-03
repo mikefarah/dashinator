513b71d54ba9bd46a106ca247d0ba62ec950fd8b
Mike Farah <mike.farah@orchestrated.io>
2016-10-03 13:12:03 +1100

    Added LESS formatter

--- precommit.sh
+++ precommit.sh
@@ -5,5 +5,8 @@ set -e
 echo 'formatting code'
 ./scripts/format-code.sh
 
+echo 'formatting less'
+./scripts/format-less.sh
+
 echo 'linting'
 ./scripts/lint-code.sh
---
+++ scripts/format-less.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./node_modules/.bin/cssbrush public/*.less
