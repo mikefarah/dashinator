5045bc9fdbfe9a8e8f3441aaed6196ef88357458
Mike Farah <mike.farah@orchestrated.io>
2016-10-12 20:10:58 +1100

    Removed sample code

--- common/containers/App.js
+++ common/containers/App.js
@@ -1,12 +1,6 @@
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Dashboard from '../components/Dashboard';
-import * as CounterActions from '../actions';
 
 const mapStateToProps = state => state;
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(CounterActions, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default connect(mapStateToProps)(Dashboard);
