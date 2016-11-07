403f7b9553c1d24c1221f01e63d7d93af8a83400
Mike Farah <mike.farah@orchestrated.io>
2016-11-07 14:05:51 +1100

    Added missing test for healthCheckFor

--- tests/server/healthChecksFor.test.js
+++ tests/server/healthChecksFor.test.js
@@ -34,6 +34,31 @@ describe('healthChecksFor', () => {
     ));
   });
 
+  context('service fails with a bad response', () => {
+    let error;
+
+    beforeEach(() => {
+      error = new Error('bad response');
+      error.response = {
+        statusCode: 503,
+        body: 'Forbidden!',
+      };
+      requestStub.reset().rejects(error);
+    });
+
+    it('returns the error message', () => healthCheck()
+      .then(state => expect(state).toEqual({
+        results: [
+          {
+            name: 'my service',
+            status: '503 - Forbidden!',
+            url: 'http://blah',
+          }],
+        description: 'Monitoring 1 service(s)',
+      })
+    ));
+  });
+
   context('service fails', () => {
     beforeEach(() => {
       requestStub.reset().rejects(new Error('no'));
